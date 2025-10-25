197c6144a15d0cdf3885dfec6481b155090edf01
Godjira <Godjira@users.noreply.github.com>
2025-10-25 18:21:57 +0000

    deploy: a20aebc892ade9c4ee5fd67a5a0f526f8ff51321

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "951500202"
+    serviceWorkerVersion: "3832151635"
   }
 });
